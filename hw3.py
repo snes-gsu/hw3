280c5bc28c5b18f0b5c5be38e67347b063ae4c38
Sade Nesmith <snesmith5@student.gsu.edu>
2022-01-27 12:28:49 -0500

    task 4 and 5 complete

--- hw3.py
+++ hw3.py
@@ -1,5 +1,8 @@
 
 from asyncio.windows_events import NULL
+from platform import win32_edition
+from tkinter.messagebox import RETRY; win32_edition
+import os
 #---Task 1---
 # Returns True if the input string has more vowels than consonants
 # Returns False if the input string has more consonants than vowels
@@ -30,15 +33,39 @@ print(cylinder_volume(3,5))
 
 #---Task 3---
 # Comma-separated values (CSV) is a popular format for storing data. For the first step of the CSV portion of this assignment, write a function that takes a list of strings as inputs, and returns a single string created by joining all the input strings together, with a comma separating them.
-
+def csv_strings(names):
+    names = ['Sade', 'John', 'Ahmed']
+    ','.join(names)
+    return names
+print(csv_strings(['Josh', 'Chris', 'Jacob']))
 #---Task 4---
-# Now write another function that takes a list of lists of strings, applies the operation from question 3 to each list, and writes the result as a row of an output file. The function should return the path to the file where the strings were written.
+# Now write another function that takes a list of lists of strings, 
+# applies the operation from question 3 to each list, and writes the result as a row 
+# of an output file. The function should return the path to the file where the strings were written.
+def abspath(list_strings):
+    list_strings = [['s', 'i', 'd', 'r'],['e', 'f'],['g', 'h', 'i']]
+#using join function to combine lists
+#create file and path and returning path where strings were written
+    ' '.join(list_strings)
+    file = open('list_strings', 'a')
+    file.write("I am writing to a file")
+    file.close()
+    return file
 
 #---Task 5---
-# Finally, write the reverse of question 4: write a function that takes in a filename (which we will assume is a CSV), and returns a list of lists of strings, where one row in the file corresponds to one list in the output list (and each value between the commas in the file row is one element in the list).
+# Finally, write the reverse of question 4: write a function that takes in a filename (which we will assume is a CSV), and 
+# returns a list of lists of strings, where one row in the file corresponds to one 
+# list in the output list (and each value between the commas in the file row is one element in the list).
+def reverse(csv):
+    csv = [['s', 'i', 'd', 'r'],['e', 'f'],['g', 'h', 'i']]
+    ','.join(csv)
+    file = open('csv', 'r')
+    print(file.read())
+    return csv
 
 #---Task 6---
 # Error-handling is an important part of writing web apps, especially when your app talks to potentially unreliable third-party APIs. This problem will have you practice the try/except keywords in Python. Write a function that takes two numbers and divides the first one by the second. You should catch the error if the second number is zero and print a warning instead of crashing.
+
 
 #---Task 7---
 # Write a function that takes a list of integers and returns the same list, but without any duplicates.
